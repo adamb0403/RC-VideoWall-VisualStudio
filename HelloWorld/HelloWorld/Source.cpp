acaa5945ba610283f87a56619352f25a547e867a
Adam Butt <adamskibutt@gmail.com>
2022-04-03 00:57:13 +0100

    Commented code

--- HelloWorld/HelloWorld/Source.cpp
+++ HelloWorld/HelloWorld/Source.cpp
@@ -29,30 +29,30 @@ void main(void)
 void convert_image(int* x1)
 {
 	int i, j, y = *x1;
-	char output_fn[100]; // Initialise the output file name array
-	char r_hex[2];
+	char output_fn[100]; // Initialise the output file name (.txt) array
+	char r_hex[2]; // Initialise arrays to store colour channel values
 	char g_hex[2];
 	char b_hex[2];
-	string images[50];
-	Mat image; //taking an image matrix//
+	string images[50]; // Initialise images file name array
+	Mat image; // Initialise Mat as an image
 
-	for (y = 1; y < *x1 + 1; y++)
+	for (y = 1; y < *x1 + 1; y++) // Iterate for all images defined
 	{
 		stringstream img;
-		img << y << "\.png" << endl;
+		img << y << "\.png" << endl; // Append .png to the image number
 
 		string filename;
 		img >> filename;
-		images[y] = filename;
+		images[y] = filename; // Store the image filenames in array
 	}
 
-	for (y = 1; y < *x1 + 1; y++)
+	for (y = 1; y < *x1 + 1; y++) // Iterate for all images defined
 	{
-		sprintf_s(output_fn, "D:/%d", y);
+		sprintf_s(output_fn, "D:/%d", y); // Create output filename
 		strcat_s(output_fn, ".txt");
 
 		FILE* output_file;
-		output_file = fopen(output_fn, "w");
+		output_file = fopen(output_fn, "w"); // Open output file
 
 		if (output_file == NULL)
 		{
@@ -60,26 +60,24 @@ void convert_image(int* x1)
 			exit(1);
 		}
 
-		for (i = 0; i < 32; i++)
+		for (i = 0; i < 32; i++) // Iterate for all pixels
 		{
 			for (j = 0; j < 32; j++)
 			{
-				image = imread(images[y]);//loading an image//
-				int b = image.at<Vec3b>(i, j)[0];//getting the pixel values//
-				int g = image.at<Vec3b>(i, j)[1];//getting the pixel values//
-				int r = image.at<Vec3b>(i, j)[2];//getting the pixel values//
+				image = imread(images[y]); // Load image into Mat "image"
+				int b = image.at<Vec3b>(i, j)[0]; // Read blue channel value
+				int g = image.at<Vec3b>(i, j)[1]; // Read green channel value
+				int r = image.at<Vec3b>(i, j)[2]; // Read red channel value
 
-				r = r / 16;
+				r = r / 16; // Convert 24 bit colour to 12 bit colour by dividing each channel by 16
 				g = g / 16;
 				b = b / 16;
 
-
-
-				sprintf_s(r_hex, "%X", r);
+				sprintf_s(r_hex, "%X", r); // Convert colour values to hex digits as string
 				sprintf_s(g_hex, "%X", g);
 				sprintf_s(b_hex, "%X", b);
 
-				fputs(r_hex, output_file);
+				fputs(r_hex, output_file); // Output colours to txt file
 				fputs(g_hex, output_file);
 				fputs(b_hex, output_file);
 			}
@@ -95,16 +93,16 @@ void imgdata(int* x1)
 	char s_time[20], totalimg[20];
 	FILE* imgdata;
 
-	sprintf_s(totalimg, "%X", *x1);
+	sprintf_s(totalimg, "%X", *x1); // Store the number of images as a hex digit in char array
 
 	printf_s("\nHow many seconds do you want each image to show for (Max 15)? (If converting only one image, press 1): ");
 	fflush(stdin);
 	scanf_s("%d", &slide_time);
-	sprintf_s(s_time, "%X", slide_time);
+	sprintf_s(s_time, "%X", slide_time); // Store the slide time as a hex digit in char array
 
-	imgdata = fopen("D:/imgdata.txt", "w");
+	imgdata = fopen("D:/imgdata.txt", "w"); // Open imagedata file
 
-	fputs(totalimg, imgdata);
+	fputs(totalimg, imgdata); // Output imagedata to file
 	fputs(s_time, imgdata);
 
 	fclose(imgdata);
